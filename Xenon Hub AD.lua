65c927fd5021f70e752da9fe5e6dba3a8e786671
Norataisen <142599223+NoraTaiSen@users.noreply.github.com>
2024-06-20 17:27:18 +0700

    Create Xenon Hub AD.lua

---
+++ Xenon Hub AD.lua
@@ -0,0 +1,9 @@
+getgenv().Configuration = {
+        ['Enabled'] = true,
+        ['Enabled Challenge'] = true,
+        ['PC Name'] = 'Xenon Hub 1',
+        ['Delay'] = 15, -- Seconds
+        ['Leave Delay'] = 5,
+        ['Leave At Wave'] = 31,
+        ['Leave Method'] = 2, -- 1 = Sell,  2 = Leave
+    };
